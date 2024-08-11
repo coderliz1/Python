29721ee3ea9c7b88446d654ced1eba209f62765f
Lizzy <coderliz1@gmail.com>
2024-08-11 18:35:52 -0400

    nested ifs

---
+++ BeginnerPython/nested-if-08.py
@@ -0,0 +1,39 @@
+#languages = ["Python", "C++", "Java", "Perl", "C#"]
+
+#for language in languages:
+    #if language == "C#":
+        #print(language + " found")
+        #break
+
+#for language in languages:
+    #if language == "C#":
+        #print(language + " found")
+    #break
+    # 
+#logging = False
+#logging_in = True
+#name = "Caleb"
+
+#if logging_in:
+    #if logging:
+        #print(name + " is logging in")
+    #print("Welcome, " + name)
+
+#age = 40
+#fun = False
+#likes_to_dance = True
+
+
+
+#if (age < 30 or fun):
+    #if likes_to_dance:
+        #print("You're invited!")
+    #else:
+        #print("OH NO, you can't dance?")
+#else:
+    #print("You're not invited....sorrytyyy")
+
+for i in range(4):
+    for j in range(5):
+        print(j, end=" ")
+    print()
