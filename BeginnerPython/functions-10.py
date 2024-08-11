74489d6cdaec1e4e4db3f085c1fe1852f4c4c094
Lizzy <coderliz1@gmail.com>
2024-08-11 19:27:27 -0400

    functions 10 python beginners

---
+++ BeginnerPython/functions-10.py
@@ -0,0 +1,13 @@
+def greet(name):
+    if name == "Claire":
+        print("You smell like pickles")
+        return
+    print("Hey there")
+    print("Welcome," + name)
+
+
+
+
+greet("Caleb")
+greet("Liz")
+greet("Claire")
