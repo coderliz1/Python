7c1dd9be45e48bd3fafb6b3c1661058e668a6ea4
Lizzy <coderliz1@gmail.com>
2024-08-13 16:55:19 -0400

    2nd part of Calebs Python tutorials

--- IntermediatePython/01-intro.py
+++ IntermediatePython/01-intro.py
@@ -1,7 +1,15 @@
+#healthy = ["kale chips", "broccoli"]
+
+#backpack = ["pizza", "frozen custard", "apple crisp", "kale chips" ]
+
+#backpack.remove("pizza")
+
+#print(backpack)
+
 healthy = ["kale chips", "broccoli"]
 
 backpack = ["pizza", "frozen custard", "apple crisp", "kale chips" ]
 
-backpack.remove("pizza")
+backpack = [item for item in backpack if item in healthy]
 
 print(backpack)
