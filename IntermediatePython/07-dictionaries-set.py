98bf702f007e3f4026e15543ea86b6d1702706eb
Lizzy <coderliz1@gmail.com>
2024-08-17 12:24:26 -0400

    calebs python tutorials

---
+++ IntermediatePython/07-dictionaries-set.py
@@ -0,0 +1,14 @@
+emails = {
+    "caleb":"caleb@email.com",
+    "gal": "g@email.com "
+}
+
+
+
+#dictionaries are good for fast look ups
+
+# how to add data to a dictionary
+
+emails.update(josh = "j@email.com")
+
+print(emails)
