b162c85adf372571d883b62e4b662864e0ecf8ba
Lizzy <coderliz1@gmail.com>
2024-08-20 14:31:12 -0400

    calebs python tutorials

--- IntermediatePython/11-tk-module.py
+++ IntermediatePython/11-tk-module.py
@@ -1,7 +1,33 @@
 import tkinter
 from random import randint
+from tkinter import messagebox
+
+low = 0
+high = 20
+rand = randint(low, high)
+print(rand)
+
+def check(guess):
+    if guess < rand:
+        tkinter.Label(tk, text=f"{guess} is too low").pack()
+    elif guess > rand:
+        tkinter.Label(tk, text=f"{guess} is too high").pack()
+    
+    else:
+        tkinter.messagebox.showinfo("You WIN!" f"{guess} is correct")
 
 tk = tkinter.Tk()
 tk.title("Guessing Game")
+
+label = tkinter.Label(tk, text=f"Guess a number {low} to {high} (inclusive)")
+label.pack()
+
+entry = tkinter.Entry(tk)
+entry.pack()
+
+button = tkinter.Button(tk, text="Guess", command=lambda: check(int(entry.get())))
+button.pack()
+
+
 tk.mainloop()
 
