2c93d72ada958f0d438f77bc6a037de688e84a01
Lizzy <coderliz1@gmail.com>
2024-08-20 20:34:51 -0400

    calebs python tutorial

--- IntermediatePython/11-tk-listBox.py
+++ IntermediatePython/11-tk-listBox.py
@@ -17,32 +17,50 @@
 
 
 import tkinter
-from tkinter import END
-
-def add_to_list():
-    listbox.insert(END, entry.get())
-    entry.delete(0, END)
-
-def remove_from_list():
-    listbox.delete(listbox.curselection())
-    
-
-
+from book import Book
+import booksSDK
 
 tk = tkinter.Tk()
-tk.title("Listbox")
+books = []
 
-listbox = tkinter.Listbox(tk)
-listbox.pack()
+def add_to_list():
 
-entry = tkinter.Entry(tk)
-entry.pack()
+    if title.get() == "" or pages.get() == "":
+        return
 
-button = tkinter.Button(tk, text="Add Value", command=add_to_list)
-button.pack()
+    book = Book(title.get(), int(pages.get()))
+    print(book)
+    if(booksSDK.add_book(book)):
+        books.append(book)
+        lb.insert(tkinter.END, book)
+    title.delete(0, tkinter.END)
+    pages.delete(0, tkinter.END)
 
-button = tkinter.Button(tk, text="Remove Selected Value", command=remove_from_list)
-button.pack()
+def delete_from_list():
+    index_tuple = lb.curselection()
+    if booksSDK.delete_book(books.pop(index_tuple[0])):
+        lb.delete((index_tuple))
 
+lb = tkinter.Listbox(tk)
+
+for book in booksSDK.get_books():
+    books.append(book)
+    lb.insert(tkinter.END, book)
+
+lb.pack()
+
+add = tkinter.Button(tk, text="Add Book", command=add_to_list)
+add.pack()
+
+delete = tkinter.Button(tk, text="Delete Book", command=delete_from_list)
+delete.pack()
+
+tkinter.Label(tk, text="Title:").pack()
+title = tkinter.Entry(tk, text="Title to Add:")
+title.pack()
+
+tkinter.Label(tk, text="Pages:").pack()
+pages = tkinter.Entry(tk, text="Number of pages:")
+pages.pack()
 
 tk.mainloop()
