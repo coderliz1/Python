7cee224343b8fb969eef70103cdb6f7c9aed6c53
Lizzy <coderliz1@gmail.com>
2024-08-20 20:19:19 -0400

    calebs python tutorials

--- IntermediatePython/11-tk-listBox.py
+++ IntermediatePython/11-tk-listBox.py
@@ -1,5 +1,31 @@
+#import tkinter
+#from tkinter import END
+
+
+
+#tk = tkinter.Tk()
+#tk.title("Listbox")
+
+#listbox = tkinter.Listbox(tk)
+#listbox.pack()
+#listbox.insert(END, "hello", "Hi", "Yo")
+#listbox.insert(END, "hola", "ciao", "ca va")
+#listbox.delete(0)
+
+
+#tk.mainloop()
+
+
 import tkinter
 from tkinter import END
+
+def add_to_list():
+    listbox.insert(END, entry.get())
+    entry.delete(0, END)
+
+def remove_from_list():
+    listbox.delete(listbox.curselection())
+    
 
 
 
@@ -8,9 +34,15 @@ tk.title("Listbox")
 
 listbox = tkinter.Listbox(tk)
 listbox.pack()
-listbox.insert(END, "hello", "Hi", "Yo")
-listbox.insert(END, "hola", "ciao", "ca va")
-listbox.delete(0)
+
+entry = tkinter.Entry(tk)
+entry.pack()
+
+button = tkinter.Button(tk, text="Add Value", command=add_to_list)
+button.pack()
+
+button = tkinter.Button(tk, text="Remove Selected Value", command=remove_from_list)
+button.pack()
 
 
 tk.mainloop()
